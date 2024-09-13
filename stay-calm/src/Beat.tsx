2d504efe48da8a14236c81bef505f6c5206e338a
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2024-09-12 21:08:51 -0400

    forwardRef

--- stay-calm/src/Beat.tsx
+++ stay-calm/src/Beat.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, forwardRef } from "react";
 
 interface BeatProps {
 	type: string;
 	animationSpeed: string;
 }
 
-export default function Beat({ type, animationSpeed }: BeatProps) {
+const Beat = forwardRef<HTMLDivElement, BeatProps>(({ type, animationSpeed }: BeatProps, ref) => {
 	const [dimensions, setDimensions] = useState<{ width: string; margin: string }>({
 		width: "75px",
 		margin: "3px",
@@ -43,7 +43,7 @@ export default function Beat({ type, animationSpeed }: BeatProps) {
 				width = "50px";
 				margin = "4px";
 			} else if (type === "medium") {
-				width = "70px";
+				width = "65px";
 				margin = "3px";
 			} else if (type === "large") {
 				width = "125px";
@@ -63,18 +63,18 @@ export default function Beat({ type, animationSpeed }: BeatProps) {
 	}, [type]);
 
 	return (
-		<>
-			<div className="beat" style={{ width: dimensions.width, margin: dimensions.margin, animation: `translation ${animationSpeed} linear` }}>
-				<svg version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="150px" height="73px" viewBox="0 0 150 73" enable-background="new 0 0 150 73" xml:space="preserve">
-					<polyline
-						fill="none"
-						stroke="#00BFFF"
-						stroke-width="3"
-						stroke-miterlimit="10"
-						points="0,45.486 38.514,45.486 44.595,33.324 50.676,45.486 57.771,45.486 62.838,55.622 71.959,9 80.067,63.729 84.122,45.486 97.297,45.486 103.379,40.419 110.473,45.486 150,45.486"
-					/>
-				</svg>
-			</div>
-		</>
+		<div className="beat" ref={ref} style={{ width: dimensions.width, margin: dimensions.margin, animation: `translation ${animationSpeed} linear` }}>
+			<svg version="1.0" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="150px" height="73px" viewBox="0 0 150 73" enableBackground="new 0 0 150 73" xmlSpace="preserve">
+				<polyline
+					fill="none"
+					stroke="#00BFFF"
+					strokeWidth="3"
+					strokeMiterlimit="10"
+					points="0,45.486 38.514,45.486 44.595,33.324 50.676,45.486 57.771,45.486 62.838,55.622 71.959,9 80.067,63.729 84.122,45.486 97.297,45.486 103.379,40.419 110.473,45.486 150,45.486"
+				/>
+			</svg>
+		</div>
 	);
-}
+});
+
+export default Beat;
